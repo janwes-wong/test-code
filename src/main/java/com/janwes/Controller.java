53ff8c90b05bc8503f9819ff9152cd25501571a3
janwes <hzwjanwes@outlook.com>
2021-05-12 16:10:00 +0800

    1

---
+++ src/main/java/com/janwes/Controller.java
@@ -0,0 +1,14 @@
+package com.janwes;
+
+/**
+ * @author Janwes
+ * @version 1.0
+ * @package com.janwes
+ * @date 2021/5/12 16:07
+ * @description
+ */
+public class Controller {
+    public void save() {
+        System.out.println("save");
+    }
+}
